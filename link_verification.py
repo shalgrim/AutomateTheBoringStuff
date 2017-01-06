c3d8f6d1f60bd7280555e849faadb96b26b1d264
shalgrim@hotmail.com <shalgrim@hotmail.com>
2017-01-06 10:16:02 -0800

    link_verification, and chapter 11, complete

--- link_verification.py
+++ link_verification.py
@@ -7,19 +7,43 @@ that have a 404 "Not Found" status code and print them out as broken links.
 
 import bs4
 import logging
+import os
 import requests
 
-URL = r'http://www.warnerbros.com/archive/spacejam/movie/jam.htm'
+URL = r'http://www.warnerbros.com/archive/spacejam/movie/'
 logger = logging.getLogger('automate_boring.image_site_downloader')
 
-if  __name__ == '__main__':
+if __name__ == '__main__':
+
+    os.makedirs('links', exist_ok=True)
+
     res = requests.get(URL)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    link_elems = soup.select('.r a')
+    link_elems = soup.select('a')
 
-    # TODO: go through all the link elems and download while printing not founds
-#    for link_elem in link_elems:
+    for link_elem in link_elems:
+        link_url = link_elem.get('href')
+        if link_url[:4] != 'http':
+            link_url = URL + link_url       # relative link
+        link_res = requests.get(link_url)
+
+        try:
+            link_res.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if link_res.status_code == 404:
+                print('broken link {}'.format(link_url))
+            else:
+                raise
+
+        basename = os.path.basename(link_url)
+
+        if basename:
+            basename = basename.replace('?', '')
+            with open(os.path.join('links', basename), 'wb') as f:
+                for chunk in link_res.iter_content(100000):
+                    f.write(chunk)
+
 
 
 
