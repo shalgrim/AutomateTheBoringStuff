275775e980181b6b355cebe97e15cfb78f474e24
shalgrim <scott.halgrim@optum360.com>
2017-01-25 09:07:02 -0800

    text_files_to_spreadsheet on page 293 was a piece of cake

---
+++ text_files_to_spreadsheet.py
@@ -0,0 +1,42 @@
+"""
+reads in content of directory of text files, inserts them into spreadsheet
+with one line of text per row. lines of first file go in column A, and so on
+(page 293)
+"""
+
+import logging
+import openpyxl
+import os
+from openpyxl.utils import get_column_letter
+from srhpytools_srh.options.parsers import GenArgParser
+from srhpytools_srh.util.mylogging import config_root_file_logger
+
+logger = logging.getLogger('automate_boring.text_files_to_spreadsheet')
+
+
+def main(dirname, outfile):
+    file_lines = []
+    for fn in os.listdir(dirname):
+        with open(fn) as f:
+            file_lines.append([line.strip() for line in f.readlines()])
+
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    for i, f in enumerate(file_lines):
+        col = get_column_letter(i+1)
+        for j, l in enumerate(f):
+            row = j+1
+            sheet[f'{col}{row}'] = l
+
+    wb.save(outfile)
+
+
+if __name__ == '__main__':
+    parser = GenArgParser(usage='%(prog)s -d dirname [-o outfile] [options]')
+    parser.add_argument('-d', '--dirname', help='directory of text files')
+    args = parser.parse_args()
+    config_root_file_logger(args.logfile, args.loglevel, args.logmode)
+    main(args.dirname, args.outfile)
+
+
+
